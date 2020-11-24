81930d3fb548b32fe9b44b635e58661f8f3d4b2a
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-11-24 16:36:54 +0100

    added persistance w/ localstorage

--- src/services/todo/todo.service.ts
+++ src/services/todo/todo.service.ts
@@ -11,6 +11,21 @@ export class TodoService {
     erledigt: Todo[] = [];
 
     constructor(private modalCtrl: ModalController) {
+
+        const lsTodos = localStorage.getItem('todos');
+        const lsDone = localStorage.getItem('done');
+
+        if (lsTodos){
+            this.todos = JSON.parse(lsTodos);
+        }
+        if (lsDone){
+            this.erledigt = JSON.parse(lsDone);
+        }
+    }
+
+    setLS(){
+        localStorage.setItem('todos', JSON.stringify(this.todos));
+        localStorage.setItem('done', JSON.stringify(this.erledigt));
     }
 
     async add(todo: Todo, autor: User) {
@@ -19,6 +34,7 @@ export class TodoService {
             todo.autor = autor;
             todo.zeit = new Date().getHours() + ':' + new Date().getMinutes();
             await this.todos.push(todo);
+            this.setLS();
             await this.modalCtrl.dismiss();
         } else {
             alert('du hund');
@@ -29,21 +45,27 @@ export class TodoService {
         if (todo.titel && todo.beschreibung) {
             this.todos.find(todoAusArray => todoAusArray.id === todo.id).titel = todo.titel;
             this.todos.find(todoAusArray => todoAusArray.id === todo.id).beschreibung = todo.beschreibung;
+            this.setLS();
             await this.modalCtrl.dismiss();
         }
     }
 
     async delete(todo: Todo) {
         this.todos.splice(todo.id, 1);
+        this.setLS();
     }
 
     async done(todo: Todo) {
+        todo.erledigt = true;
         this.todos.splice(todo.id, 1);
         this.erledigt.push(todo);
+        this.setLS();
     }
 
     async notDone(todo: Todo) {
+        todo.erledigt = false;
         this.erledigt.splice(todo.id, 1);
         this.todos.push(todo);
+        this.setLS();
     }
 }
