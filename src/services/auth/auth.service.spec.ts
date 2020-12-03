e2ef40b813dbbbd9a18c4d42b21640c9a17baa10
Vorname Nachname“git config --global user.name Julian <julian.hermanspahn@mni.thm.de>
2020-12-04 00:57:44 +0100

    authService tests

--- src/services/auth/auth.service.spec.ts
+++ src/services/auth/auth.service.spec.ts
@@ -1,16 +1,56 @@
 import {TestBed} from '@angular/core/testing';
 
 import {AuthService} from './auth.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {AngularFireModule} from '@angular/fire';
 
 describe('AuthService', () => {
     let service: AuthService;
+    const spy = jasmine.createSpyObj('AuthService', ['signUp', 'signIn', 'getUser', 'logOut']);
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [RouterTestingModule, HttpClientTestingModule, AngularFireModule],
+            providers: [{provide: AuthService, useValue: spy}]
+        }).compileComponents();
         service = TestBed.inject(AuthService);
     });
 
     it('should be created', () => {
         expect(service).toBeTruthy();
     });
+
+    describe('sign up', () => {
+        it('should sign up', (done) => {
+            service.signUp('jasmine', 'jasmine@karma.com', 'jasmine');
+            expect(service.user.nutzername).toBe('jasmine');
+            done();
+        });
+    });
+
+    describe('log in', () => {
+        it('should log in', (done) => {
+            service.signIn('jasmine@karma.com', 'jasmine');
+            expect(service.user.nutzername).toBe(typeof String);
+            done();
+        });
+    });
+
+    describe('get user', () => {
+        it('should get user', (done) => {
+            service.getUser();
+            expect(service.user.nutzername).toBe(typeof String);
+            done();
+        });
+    });
+
+    describe('log out', () => {
+        it('should log out', (done) => {
+            service.logOut();
+            expect(service.user.nutzername).toBe(typeof undefined);
+            done();
+        });
+    });
+
 });
