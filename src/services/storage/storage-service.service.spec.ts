a2dddc993e3fa98958669c72f710f5c1527901e7
Lewe Lorenzen <lewe.lorenzen@mni.thm.de>
2020-12-09 18:30:29 +0100

    Error in Stoarage Service Fixed storage-service.service.spec.ts

--- src/services/storage/storage-service.service.spec.ts
+++ src/services/storage/storage-service.service.spec.ts
@@ -2,13 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { StorageServiceService } from './storage-service.service';
 import {RouterTestingModule} from '@angular/router/testing';
+import {AuthService} from '../auth/auth.service';
 
 describe('StorageServiceService', () => {
   let service: StorageServiceService;
+  const spy = jasmine.createSpyObj('AuthService', ['signUp', 'signIn', 'getUser', 'logOut']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
+      providers: [{provide: AuthService, useValue: spy}]
     });
     service = TestBed.inject(StorageServiceService);
   });
--- src/services/todo/todo.service.spec.ts
+++ src/services/todo/todo.service.spec.ts
@@ -4,20 +4,23 @@ import { TodoService } from './todo.service';
 import {RouterTestingModule} from '@angular/router/testing';
 // import {ModalController} from '@ionic/angular';
 import {StorageServiceService} from '../storage/storage-service.service';
+import {AuthService} from '../auth/auth.service';
+import {ModalController} from '@ionic/angular';
 
 describe('TodoService', () => {
   let service: TodoService;
-  let storageServiceSpy: jasmine.SpyObj<StorageServiceService>;
+  const authSpy = jasmine.createSpyObj('AuthService', ['signUp', 'signIn', 'getUser', 'logOut']);
+  const modalSpy = jasmine.createSpyObj('ModalController', ['dismiss']);
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('StorageServiceService', ['getValue']);
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      providers: [TodoService,
-        { provide: StorageServiceService, useValue: spy }]
+      providers: [
+        TodoService,
+        {provide: AuthService, useValue: authSpy},
+        {provide: ModalController, useValue: modalSpy}]
     });
     service = TestBed.inject(TodoService);
-    storageServiceSpy = TestBed.inject(StorageServiceService) as jasmine.SpyObj<StorageServiceService>;
   });
 
   it('should be created', () => {
